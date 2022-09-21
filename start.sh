253e9fc4823a9bdac169f2839575fcd8e7c8f1b2
imVipin <84066351+imVipin@users.noreply.github.com>
2022-09-21 19:48:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/imVipin/findpdf.git /findpdf
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /findpdf
 fi
-cd /EvaMaria
+cd /findpdf
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
