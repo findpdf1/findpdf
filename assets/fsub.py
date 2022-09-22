248c4b7f1b5d0a6feef7a273cfc45b36299bdf74
imVipin <84066351+imVipin@users.noreply.github.com>
2022-09-22 18:49:53 +0530

    update fsub.py

--- assets/fsub.py
+++ assets/fsub.py
@@ -1,62 +1,38 @@
 import os
-import asyncio
-#from info import FORCE_SUB
-from pyrogram import Client
-from pyrogram.errors import FloodWait, UserNotParticipant
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+import datetime
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import UserNotParticipant, ChatAdminRequired, UsernameNotOccupied
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "-1001368430615")
-
-
-
-async def ForceSub(bot: Client, event: Message):
-    try:
-        invite_link = await bot.create_chat_invite_link(chat_id=(int(FORCE_SUB) if FORCE_SUB.startswith("-100") else FORCE_SUB))
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        fix_ = await ForceSub(bot, event)
-        return fix_
-    except Exception as err:
-        print(f"Unable to do Force Subscribe to {FORCE_SUB}\n\nError: {err}\n\nContact @vipinpkd")
-        return 200
-    try:
-        user = await bot.get_chat_member(chat_id=(int(FORCE_SUB) if FORCE_SUB.startswith("-100") else FORCE_SUB), user_id=event.from_user.id)
-        if user.status == "banned":
-            await bot.send_message(
-                chat_id=event.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [master](https://t.me/vipinpkd).",
-                parse_mode="markdown",
-                disable_web_page_preview=True,
-                reply_to_message_id=event.message_id
-            )
-            return 400
-        else:
-            return 200
-    except UserNotParticipant:
-        await bot.send_message(
-            chat_id=event.from_user.id,
-            text="**Please Join My PDF Channel to use this Bot!**\n\n__Due to Overload, Only Channel Subscribers can use the Bot!__",
-            reply_markup=InlineKeyboardMarkup( [[
-                InlineKeyboardButton("Join 📚PDF Channel", url=invite_link.invite_link)
-                ]]                
-            ),
-            parse_mode="markdown",
-            reply_to_message_id=event.message_id
-        )
-        return 400
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        fix_ = await ForceSub(bot, event)
-        return fix_
-    except Exception as err:
-        print(f"Something Went Wrong! Unable to do Force Subscribe.\nError: {err}\n\nContact @vipinpkd")
-        return 200
-
-
-@Client.on_message(filtes.private & filters.incoming & filters.text)
-async def forcesub(client, message):
-    FSub = await ForceSub(client, message)
-    if FSub == 400:
-        return
-
+FORCE_SUB = os.environ.get("FORCE_SUB", "Pdfmalayalam") if os.environ.get("FORCE_SUB", "") else None
   
+@Client.on_message(filters.private & filters.incoming)
+async def force_sub(c, m):
+    if FORCE_SUB:
+        try:
+            chat = await c.get_chat_member(FORCE_SUB, m.from_user.id)
+            if chat.status=='kicked':
+                return await m.reply_text('Hai you are kicked from my updates channel. So, you are not able to use me',  quote=True)
+
+        except UserNotParticipant:
+            button = [[InlineKeyboardButton('join Updates channel', url=f'https://t.me/Pdfmalayalam')]]
+            markup = InlineKeyboardMarkup(button)
+            return await m.reply_text(text="Hey join in my updates channel to use me.", parse_mode='markdown', reply_markup=markup, quote=True)
+
+        except ChatAdminRequired:
+            #logger.warning(f"Make me admin in @{FORCE_SUB}")
+            if m.from_user.id in Config.AUTH_USERS:
+                return await m.reply_text(f"Make me admin in @{FORCE_SUB}")
+
+        except UsernameNotOccupied:
+            #logger.warning("The forcesub username was Incorrect. Please give the correct username.")
+            if m.from_user.id in Config.AUTH_USERS:
+                return await m.reply_text("The forcesub username was Incorrect. Please give the correct username.")
+
+        except Exception as e:
+            if "belongs to a user" in str(e):
+                #logger.warning("Forcesub username must be a channel username Not yours or any other users username")
+                if m.from_user.id in Config.AUTH_USERS:
+                    return await m.reply_text("Forcesub username must be a channel username Not yours or any other users username")
+            #logger.error(e)
+            return await m.reply_text("Some thing went wrong. Try again and if same issue occur contact [Him](https://t.me/vipinpkd)", disable_web_page_preview=True, quote=True)
