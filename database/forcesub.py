5327664105843f2184e5a4ad286ebe8c130b7a7f
imVipin <84066351+imVipin@users.noreply.github.com>
2022-09-23 11:36:33 +0530

    update forcesub.py

--- database/forcesub.py
+++ database/forcesub.py
@@ -1,75 +1,84 @@
+#https://github.com/M299792458/chatSizeBot
 import os
-import asyncio
-from pyrogram import Client, filters, __version__
-from pyrogram.enums import ParseMode
-from pyrogram.enums import ChatMemberStatus
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
-from info import ADMINS
+import time
+from pyrogram.enums.parse_mode import ParseMode
+#from bot import LOGGER
+#from config import Config
+from pyrogram.errors import FloodWait, UserNotParticipant
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
-#from config import ADMINS, FORCE_MSG, START_MSG, OWNER_ID, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
-#from helper_func import subscribed, encode, decode, get_messages
-#from database.sql import add_user, query_msg, full_userbase
+class Config:
+    FORCE_SUBSCRIBE_CHANNEL = os.environ.get('FORCE_SUBSCRIBE_CHANNEL','Pdfmalayalam') # force subscribe channel link.
+    CHANNEL_OR_CONTACT = os.environ.get('CHANNEL_OR_CONTACT', "vipinpkd") # give your public channel or contact username
 
-
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001368430615"))
-
-FORCE_MSG = """Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>"""
-
-#WAIT_MSG = """"<b>Processing ...</b>"""
-
-#REPLY_ERROR = """<code>Use this command as a replay to any telegram message with out any spaces.</code>"""
-
-
-async def is_subscribed(filter, client, update):
-    if not FORCE_SUB_CHANNEL:
-        return True
-    user_id = update.from_user.id
-    if user_id in ADMINS:
-        return True
-    try:
-        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL, user_id = user_id)
-    except UserNotParticipant:
-        return False
-
-    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
-        return False
-    else:
-        return True
-
-subscribed = filters.create(is_subscribed)
-
-@Client.on_message(filters.command('help') & filters.private)
-async def not_joined(client: Client, message: Message):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                "Join Channel",
-                url = client.invitelink)
-        ]
-    ]
-    try:
-        buttons.append(
-            [
-                InlineKeyboardButton(
-                    text = 'Try Again',
-                    url = f"https://t.me/{client.username}?start={message.command[1]}"
+    JOIN_CHANNEL_STR = os.environ.get('JOIN_CHANNEL_STR',
+        "Hi {}\n\n" + \
+        "First Subscribe my Channel from Button, Try Again.\n" + \
+        "Due to Overload, Only Channel Subscribers Can Use This Bot.")
+    YOU_ARE_BANNED_STR = os.environ.get('YOU_ARE_BANNED_STR',
+        "You are Banned to use me.\n\nfor further queries : {}")
+    JOIN_BUTTON_STR = os.environ.get('JOIN_BUTTON_STR', "Join Our 📚PDF Channel")
+ #-----------------------------#   
+    
+def ForceSub(event: Message):
+    """
+    Custom Pyrogram Based Telegram Bot's Force Subscribe Function by @viharasenindu.
+    If User is not Joined Force Sub Channel Bot to Send a Message & ask him to Join First.
+    
+    :param bot: Pass Client.
+    :param event: Pass Message.
+    :return: It will return 200 if Successfully Got User in Force Sub Channel and 400 if Found that User Not Participant in Force Sub Channel or User is Kicked from Force Sub Channel it will return 400. Also it returns 200 if Unable to Find Channel.
+    """
+    if Config.FORCE_SUBSCRIBE_CHANNEL is not None:
+        try:
+            invite_link = event._client.create_chat_invite_link(
+                chat_id=(
+                    int(Config.FORCE_SUBSCRIBE_CHANNEL) if Config.FORCE_SUBSCRIBE_CHANNEL.startswith("-100") else Config.FORCE_SUBSCRIBE_CHANNEL
+                ),
+                member_limit = 1
+            )
+        except FloodWait as e:
+            time.sleep(e.value)
+            fix_ = ForceSub(event)
+            return fix_
+        except Exception as err:
+            #LOGGER.error(f"Error: {err}\nDo not forget to make admin your bot in forcesub channel.\nDestek / Support: {Config.CHANNEL_OR_CONTACT}") # debug
+            return 200
+        try:
+            user = event._client.get_chat_member(chat_id=(int(Config.FORCE_SUBSCRIBE_CHANNEL) if Config.FORCE_SUBSCRIBE_CHANNEL.startswith("-100") else Config.FORCE_SUBSCRIBE_CHANNEL), user_id=event.from_user.id)
+            if user.status == "kicked":
+                event.reply_text(
+                    text=Config.YOU_ARE_BANNED_STR.format(Config.CHANNEL_OR_CONTACT),
+                    parse_mode = ParseMode.HTML,
+                    disable_notification=True,
+                    disable_web_page_preview=True,
+                    reply_to_message_id = event.id
                 )
-            ]
-        )
-    except IndexError:
-        pass
-
-    await message.reply(
-        text = FORCE_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-        reply_markup = InlineKeyboardMarkup(buttons),
-        quote = True,
-        disable_web_page_preview = True
-    )
-
+                return 400
+            else:
+                return 200
+        except UserNotParticipant:
+            event.reply_text(
+                text=Config.JOIN_CHANNEL_STR.format(event.from_user.mention),
+                parse_mode = ParseMode.HTML,
+                disable_notification=True,
+                reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(f"{Config.JOIN_BUTTON_STR}", url=invite_link.invite_link)
+                    ]
+                ]
+                ),
+                disable_web_page_preview=True,
+                reply_to_message_id = event.id
+            )
+            return 400
+        except FloodWait as e:
+            time.sleep(e.value)
+            fix_ = ForceSub(event)
+            return fix_
+        except Exception as err:
+            #LOGGER.error(f"Error: {err}\nDo not forget to make admin your bot in forcesub channel.\nDestek / Support: {Config.CHANNEL_OR_CONTACT}") # debug
+            return 200
+    else:
+        return 200
