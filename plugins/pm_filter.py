37c9ccb19508f9dceb129e4dbd8cded003745317
imVipin <84066351+imVipin@users.noreply.github.com>
2022-09-24 20:27:32 +0530

    update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -721,7 +721,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"Here is what i found for your query {search} \n\n join @pdfmalayalam"
+        cap = f"Here is what i found for your \nQuery: {search} \n\n join @pdfmalayalam"
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
