1c827975156c552b548fac3e5127e19e24ea743d
findpdf1 <116649204+findpdf1@users.noreply.github.com>
2022-10-25 18:26:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/imVipin/findpdf.git /findpdf
+  git clone https://github.com/findpdf1/findpdf.git /findpdf
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /findpdf
